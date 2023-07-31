18a5debcead263cf0ea472b8933f09962fc59bce
shubham0925 <shubham25khirwadkar@gmail.com>
2023-07-30 21:15:52 -0500

    Model change to text as series can exceed varchar length

--- backend/fibonacci_series/fibonacci_series_generator/models/fibonacci_series_model.py
+++ backend/fibonacci_series/fibonacci_series_generator/models/fibonacci_series_model.py
@@ -1,9 +1,16 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class FibonacciSeries(models.Model):
     number = models.BigIntegerField(primary_key=True)
-    series = models.CharField(max_length=2000, blank=True, null=True)
+    series = models.TextField()
 
     class Meta:
         managed = False
